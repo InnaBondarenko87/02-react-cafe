a2430c6caa5dbd6c115fea4de2e5a15bd4965a76
Inna Bondarenko <bondarenko211287@gmail.com>
2025-05-14 23:54:48 +0200

    app

---
+++ src/components/App/App.tsx
@@ -0,0 +1,12 @@
+// import { useState } from "react";
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+
+// import React from "react";
+import css from "./App.module.css";
+
+function App() {
+  return <div className={css.app}></div>;
+}
+
+export default App;
