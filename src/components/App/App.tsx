55a85843b3279e05581d2d052b373728b0e57c56
Inna Bondarenko <bondarenko211287@gmail.com>
2025-05-15 21:34:37 +0200

    VoteOptions

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,16 +1,34 @@
-// import { useState } from "react";
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-
-// import React from "react";
+import { useState } from "react";
 import css from "./App.module.css";
 import CafeInfo from "../CafeInfo/CafeInfo";
+import VoteOptions from "../VoteOptions/VoteOptions";
+import type { Votes, VoteType } from "../../types/votes";
 
 function App() {
+  const [_votes, setVotes] = useState<Votes>({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  });
+
+  const handleVote = (type: VoteType) => {
+    setVotes((prevVotes) => ({
+      ...prevVotes,
+      [type]: prevVotes[type] + 1,
+    }));
+  };
+
+  const resetVotes = () => {
+    setVotes({ good: 0, neutral: 0, bad: 0 });
+  };
   return (
     <div className={css.app}>
       <div>
         <CafeInfo />
+      </div>
+      <div>
+        <VoteOptions onVote={handleVote} onReset={resetVotes} canReset={true} />
+        {/* Тут у майбутньому додамо VoteStats */}
       </div>
     </div>
   );
--- src/types/votes.ts
+++ src/types/votes.ts
@@ -1,4 +1,9 @@
-// declare module "*.module.css" {
-//   const classes: { [key: string]: string };
-//   export default classes;
-// }
+// Тип для ключів голосів
+export type VoteType = "good" | "neutral" | "bad";
+
+// Інтерфейс для об'єкта голосів
+export interface Votes {
+  good: number;
+  neutral: number;
+  bad: number;
+}
